9a7386215f2e2a2b150a325ddfa4372065a86dce
ayama2123 <a.yamazaki2123@gmail.com>
2024-07-24 00:08:25 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,22 +13,23 @@ def extract_text_from_image(image):
 
 # ChatGPTを使ってテキストから給与や勤務地を解析する関数
 def parse_job_info_with_gpt(text):
+    prompt = f"""
+    {text}で取得した情報から、以下の項目を取得します。また、それぞれの項目ごとに10代が働くことを想定した場合の評価を出力します。
+    給与
+    勤務地
+    必要な資格・スキル
+    年間休日数
+    ボーナスの有無
+    福利厚生
+    交通費や賃料の補助
+    給与や勤務地の情報を抽出してください。\n\n{text}\n\n給与情報:\n勤務地情報:",
+    """
+    
     response = openai.chat.completions.create(
         engine="gpt-4o-mini",
         messages=[
             {"role": "system", "content": f"あなたは求人情報から必要な情報を抜き出すGPTです。"},
             {"role": "user", "content": prompt},
-        prompt = f"""
-        {text}で取得した情報から、以下の項目を取得します。また、それぞれの項目ごとに10代が働くことを想定した場合の評価を出力します。
-        給与
-        勤務地
-        必要な資格・スキル
-        年間休日数
-        ボーナスの有無
-        福利厚生
-        交通費や賃料の補助
-        給与や勤務地の情報を抽出してください。\n\n{text}\n\n給与情報:\n勤務地情報:",
-        """
         max_tokens=1500
         ]
     )
